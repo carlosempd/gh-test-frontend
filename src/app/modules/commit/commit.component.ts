459dac4727548ed615ef1c0fc0de7a46e3dfb769
Carlos <carlosempd@gmail.com>
2023-08-24 14:33:21 -0400

    ✨ feat: Add a filter input to the commits list

--- src/app/modules/commit/commit.component.ts
+++ src/app/modules/commit/commit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
 import { Commit } from 'src/app/core/models/commit.model';
 import { CommitService } from 'src/app/core/services/commit.service';
 
@@ -11,17 +12,25 @@ import { CommitService } from 'src/app/core/services/commit.service';
 })
 export class CommitComponent implements OnInit {
   selected = new FormControl(0); // Tab index selected, 0 for frontend 1 for backend
-  data: Commit[] = [];
-  ctx = { commitsContext: this.data };
+
+  dataSource = new MatTableDataSource<Commit>([])
+  ctx = { commitsContext: this.dataSource };
   displayedColumns: string[] = ['sha', 'message', 'date', 'author'];
   loading = false;
+  filterForm!: FormGroup;
 
-  constructor(private commitService: CommitService) {
+  constructor(
+    private commitService: CommitService,
+    private formBuilder: FormBuilder
+  ) {
     this.selected.valueChanges.subscribe(res => {
       this.getCommits();
     })
   }
   ngOnInit(): void {
+    this.filterForm = this.formBuilder.group({
+      filter: ['']
+    })
     this.getCommits();
   }
 
@@ -29,11 +38,16 @@ export class CommitComponent implements OnInit {
     const repository = this.selected.value ? 'backend': 'frontend';
     this.loading = true;
     this.commitService.getCommitList(repository).subscribe(res => {
-        this.loading = false;
-        this.data = res;
-        this.ctx = { commitsContext: this.data };
+      this.loading = false;
+      this.dataSource.data = res;
+      this.ctx = { commitsContext: this.dataSource };
         
-    })
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,3 @@
-export const environment = {};
+export const environment = {
+    apiUrl: 'https://api-commit-history.onrender.com'
+};
